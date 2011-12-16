308d97d728d3779a8e27d930844813df690718e0
Ery <ery@baoleihang.com>
2011-12-16 15:53:13 +0800

    show_paginate_renderer

--- lib/view_builder/show_paginate_renderer.rb
+++ lib/view_builder/show_paginate_renderer.rb
@@ -1,3 +1,6 @@
+require 'will_paginate'
+require 'will_paginate/view_helpers/action_view'
+
 module ViewBuilder
   class ShowPaginateRenderer < WillPaginate::ActionView::LinkRenderer
     def to_html
