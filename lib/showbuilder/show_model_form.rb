827b931c7a3e02a21a3430a77cb42a095c9fbee6
Ery <ery@baoleihang.com>
2012-02-23 03:55:59 +0800

    update show_model_form

--- lib/showbuilder/show_model_form.rb
+++ lib/showbuilder/show_model_form.rb
@@ -21,32 +21,22 @@ module Showbuilder
       options.merge!(:builder => Showbuilder::Builders::ModelFormBuilder)
       self.form_for(models, options) do |form|
         self.html_contents do |contents|
-          contents << self.error_messages_for(models.last)
+          contents << self.model_error_messages(models.last)
           contents << capture(form, &block)
         end
       end
     end
     
-    def error_messages_for(*objects)
-      options = objects.extract_options!
-      options[:header_message]  ||= show_itext("activerecord.errors.header", :default => "Invalid Fields")
-      options[:message]         ||= show_itext("activerecord.errors.message", :default => "Correct the following errors and try again.")
-
-      messages = objects.compact.map do |object| 
-        object.errors.messages.map do |key, value|
-          value
-        end
+    def model_error_messages(object)
+      messages = object.errors.messages.map do |key, value|
+        value
       end
       messages.flatten!
-
-      if messages.empty?
-        return
-      end
+      return if messages.empty?
 
       contents_tag(:div, :class => "alert alert-error") do |contents|
         contents << content_tag(:a, 'x', :class=>'close' , "data-dismiss" => :alert)
-        contents << content_tag(:h3, options[:header_message], :class => "alert-heading")
-        contents << options[:message]
+        contents << content_tag(:strong, flash[:error], :class => "alert-heading") if flash[:error]
         contents << content_tag(:ul) do
           list_items = messages.map do |msg|
             content_tag(:li, msg)
