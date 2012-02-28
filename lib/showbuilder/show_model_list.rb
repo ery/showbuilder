f6f0bf791590057a585bfd3add5d59b7ef3c9db9
Ery <ery@baoleihang.com>
2012-02-28 13:32:37 +0800

    update commonds

--- lib/showbuilder/show_model_list.rb
+++ lib/showbuilder/show_model_list.rb
@@ -4,40 +4,18 @@ module Showbuilder
   module ShowModelList  
     #
     # show_model_list @products do |list|
-    #   list.show_column                :x_field
-    #   list.show_text_column           :x_field
-    #   list.show_currency_column       :x_field
-    #   list.show_percent_column        :x_field
-    #   list.show_date_column           :x_field
-    #   list.show_time_column           :x_field
-    #   list.show_text_link_column      :x_field
-    #   list.show_currency_link_column  :x_field
-    #   list.show_percent_link_column   :x_field
-    #   list.show_date_link_column      :x_field
-    #   list.show_time_link_column      :x_field
+    #   list.show_column                :some_field
+    #   list.show_text_column           :some_field
+    #   list.show_currency_column       :some_field
+    #   list.show_percent_column        :some_field
+    #   list.show_date_column           :some_field
+    #   list.show_time_column           :some_field
+    #   list.show_text_link_column      :some_field
+    #   list.show_currency_link_column  :some_field
+    #   list.show_percent_link_column   :some_field
+    #   list.show_date_link_column      :some_field
+    #   list.show_time_link_column      :some_field
     # end
-    #
-    # ---------------------------------------------------------------
-    # Demo:
-    # show_model_list @sale do |list|
-    #   list.show_text_link_column :number
-    # end
-    #
-    # Explain:
-    #   text = sale.number
-    #   link = link_to(sale)
-    # ---------------------------------------------------------------
-    #
-    # ---------------------------------------------------------------
-    # Demo:
-    # show_model_list @sale do |list|
-    #   list.show_text_link_column :member, :number
-    # end
-    #
-    # Explain:
-    #   text = sale.member.number
-    #   link = link_to(sale.member)
-    # ---------------------------------------------------------------
     #
     def show_model_list(models, text_group = nil, &block)
       builder = Showbuilder::Builders::ModelListBuilder.new(self, text_group)
