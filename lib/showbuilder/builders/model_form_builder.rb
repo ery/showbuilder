886d4c6a7006cc2af57ff0e7102b50cb52a9435a
Ery <ery@baoleihang.com>
2012-03-02 20:14:25 +0800

    update ModelFormBuilder

--- lib/showbuilder/builders/model_form_builder.rb
+++ lib/showbuilder/builders/model_form_builder.rb
@@ -45,59 +45,6 @@ module Showbuilder
         end
       end
 
-      def show_select_field(method, include_blank = false, options = nil)
-        method = method.to_s
-        choices = @object.send("#{method}_names")
-        show_select_by_number_field_core(method, choices, include_blank, options)
-      end
-
-      def show_select_by_number_field_core(method, choices, include_blank = false, html_options = nil)
-        method_for_number = "#{method}_number"
-        label_text = show_current_itext("#{method}.name")  
-
-        self.show_method_input(method_for_number, :label_text => label_text) do
-          self.select(method_for_number, choices, {:include_blank => include_blank}, html_options||{})
-        end
-      end
-
-      def show_check_box_field(method, options = {})
-        self.show_memo_input(method) do
-          contents_tag(:label, :class => :checkbox) do |contents|
-            contents << self.check_box(method, options)
-            contents << self.content_tag(:span, self.controller_itext("#{method}_detail"))
-          end
-        end
-      end
-
-      def show_prepend_check_box_field(method,options = {})
-        self.show_method_input(method) do
-          contents_tag(:div, :class => "input input-prepend") do |contents|
-            contents << content_tag(:label , :class => "add-on") do
-              check_box(method, options)
-            end
-            contents << self.text_field(:discount_percent ,:class => 'discount', :placeholder => show_itext('product.discount_example'))
-          end
-        end
-      end
-
-      def show_check_box_list_field(name, method_list, label_options = {})
-        self.show_method_input(name) do
-          html_contents do |contents|
-            method_list.each do |method|
-              contents << self.show_check_box_list_field_item(method, label_options)
-            end
-
-          end
-        end
-      end
-
-      def show_check_box_list_field_item(method, label_options = {})
-        contents_tag(:label, label_options) do |contents|
-          contents << self.check_box(method, options)
-          contents << self.content_tag(:span, self.show_current_itext("#{method}_detail")) #method_label(method, "#{method}_detail")
-        end
-      end
-
       def show_method_input(method, options = {})
         label_text = options[:label_text] || self.show_current_itext(method)
         div_options = options[:class] || ''
